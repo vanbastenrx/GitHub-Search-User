d182c16fb052509274918f735b6cc75c1130e542
vanbastenrx <vanbastendev@gmail.com>
2022-07-06 16:42:26 -0300

    media

--- Js/script.js
+++ Js/script.js
@@ -4,70 +4,81 @@ const renderUserGit = document.querySelector('.apresentacao');
 const btn = document.querySelector('.buscar');
 const input = document.querySelector('input');
 const API_URL = 'https://api.github.com/users/';
+const nome = document.querySelector('.github--nome');
+const userName = document.querySelector('.github--username');
+const bio = document.querySelector('.github--bio');
+const repo = document.querySelector('.qtd--repo');
+const followers = document.querySelector('.qtd--seguidores');
+const following = document.querySelector('.qtd--seguindo');
+const repositorio = document.querySelector('.repositorio');
+const seguidores = document.querySelector('.seguidores');
+const seguindo = document.querySelector('.seguindo');
+const apresentacao = document.querySelector('.apresentacao');
+const local = document.querySelector('.location');
+const twitter = document.querySelector('.twit');
+const portifolio = document.querySelector('.port');
+const work = document.querySelector('.work');
+let img = document.createElement('img');
+let imagem = document.querySelector('.foto--perfil');
+
 
 const githubUser = async (user) => {
   try {
+    // Chamada da API
     const response = await fetch(`${API_URL}${user}`);
+    // Convertendo pra JSON
     const data = await response.json();
     console.log(data);
+
     if (!response) throw new Error(`${message}`);
+
+    // Render no HTML
     renderUser(data);
   } catch (err) {
     console.error(err.message);
   }
 };
 
+const opac = () => {
+  apresentacao.style.opacity = 0;
+  alert('Usuário não existe');
+}
+
 const renderUser = data => {
-  const html = `<div class="githubHead">
-  <div class="foto--perfil"><img src="${data.avatar_url}"></div>
-<div class="biografia">
-  <div class="githubNome">${!data.name || null ? 'Sem Nome' : data.name}</div>
-  <div class="github--username"><a href="${data.html_url}">@${data.login}</a></div>
-</div>
-</div>
 
-<div class="gitBio">
-  <div class="github--bio">${!data.bio || null ? 'Sem biografia' : data.bio}</div>
-  <div class="github--repo">
-    <div class="repos">
-      <div class="repositorio">Repositórios</div>
-      <div class="qtd--repo">${data.public_repos}</div>
-    </div>
-    <div class="repos">
-      <div class="seguidores">Seguidores</div>
-      <div class="qtd--seguidores">${data.followers}</div>
-    </div>
-    <div class="repos">
-      <div class="seguindo">Seguindo</div>
-      <div class="qtd--seguindo">${data.following}</div>
-    </div>
-  </div>
+  const status = 'Indisponível';
+  const companyStatus = 'Freelancer';
+  const biographStatus = 'Sem descrição';
+  const repoStatus = 'repositórios';
+  const qtdFollower = 'seguidores';
+  const qtdFollowing = 'seguindo';
 
-  <div class="social--media">
-    <div class="local">
-      <img src="./imagens/location-pin.svg">
-      <p class="local">${!data.location || null ? 'Localização' : data.location}</p>
-    </div>
-    <div class="twitter">
-      <img src="./imagens/twitter.svg">
-      <p class="local"><a href="">${!data.twitter_username || null ? 'Twitter' : data.twitter_username}</a></p>
-    </div>
-    <div class="portifolio">
-      <img src="./imagens/globe.svg">
-      <p class="local"><a href="${data.blog}">Portfólio</a></p>
-    </div>
-    <div class="empresa">
-      <img style="width:2em" src="./imagens/briefcase.svg">
-      <p class="local">${!data.company || null ? 'Freelancer' : data.company}</p>
-    </div>
-  </div>
-</div>`;
-  renderUserGit.insertAdjacentHTML('afterbegin', html);
+  // Elementos do HTML com a API
+  apresentacao.style.opacity = 1;
+  img.src = data.avatar_url;
+  imagem.appendChild(img);
+  nome.innerHTML = data.name;
+  userName.innerHTML = !data.login ? opac() : `@${data.login}`;
+  bio.innerHTML = !data.bio ? biographStatus : data.bio;
+  
+  portifolio.innerHTML = data.html_url;
+  local.innerHTML = !data.location ? status : data.location;
+  work.innerHTML = !data.company ? companyStatus : data.company;
+  twitter.innerHTML = !data.twitter_username ? status : data.twitter_username;
+
+  // Strings
+  repositorio.innerHTML = repoStatus;
+  seguidores.innerHTML = qtdFollower;
+  seguindo.innerHTML = qtdFollowing;
+  repo.innerHTML = data.public_repos;
+  followers.innerHTML = data.followers;
+  following.innerHTML = data.following;
 };
 
-
+// Input + botão
 btn.addEventListener('submit', (e) => {
   e.preventDefault();
+  input.value === '' ? false :
   githubUser(input.value);
   input.value = '';
 });
